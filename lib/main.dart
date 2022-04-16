0eb24b14a63cdd4a45cffe9474597d51972726bc
teruj <tjibu@msn.com>
2022-04-16 20:25:41 +0900

    step6_finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import './poke_list_item.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+    loadThemeMode().then((val) => setState(() => themeMode = val));
+  }
 
   // This widget is the root of your application.
   @override
@@ -15,7 +32,7 @@ class MyApp extends StatelessWidget {
       title: 'Pokemon Flutter',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
+      themeMode: themeMode,
       home: const TopPage(),
     );
   }
@@ -37,28 +54,24 @@ class _TopPageState extends State<TopPage> {
       body: SafeArea(
         child: currentbnb == 0 ? const PokeList() : const Settings(),
       ),
-      bottomNavigationBar:BottomNavigationBar(
-        
-        
-        onTap: (index) => {
-          setState(() => currentbnb = index,)
-        }, 
-
-        currentIndex: currentbnb,
-
-        items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.list), label: 'home'),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'setting',
-        ),
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) => {
+                setState(
+                  () => currentbnb = index,
+                )
+              },
+          currentIndex: currentbnb,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'setting'),
+          ]),
     );
   }
 }
 
 class PokeList extends StatelessWidget {
-  const PokeList ({ Key? key }) : super(key: key);
+  const PokeList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -70,31 +83,149 @@ class PokeList extends StatelessWidget {
   }
 }
 
-class Main extends StatelessWidget {
-const Main({ Key? key }) : super(key: key);
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container();
-  }
+  State<Settings> createState() => _SettingsState();
 }
 
-class Settings extends StatelessWidget {
-const Settings({ Key? key }) : super(key: key);
+class _SettingsState extends State<Settings> {
+  ThemeMode _themeMode = ThemeMode.system;
 
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    loadThemeMode().then((val) => setState(() => _themeMode = val));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ListView(
-      children: const [
+      children: [
         ListTile(
-          leading: Icon(Icons.lightbulb),
-          title: Text('Dark/Light Mode'),
-        )
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          trailing: Text((_themeMode == ThemeMode.system)
+              ? 'System'
+              : (_themeMode == ThemeMode.dark ? 'Dark' : 'Light')),
+          onTap: () async {
+            var ret = await Navigator.of(context).push<ThemeMode>(
+              MaterialPageRoute(
+                builder: (context) => ThemeModeSelectionPage(init: _themeMode),
+              ),
+            );
+
+            setState(() => _themeMode = ret!);
+            await saveThemeMode(_themeMode);
+          },
+        ),
+        // SwitchListTile(
+        //     title: const Text('Switch'), value: true, onChanged: (yes) => {}),
+        // CheckboxListTile(
+        //   title: const Text('Checkbox'),
+        //   value: true,
+        //   onChanged: (yes) => {},
+        // ),
+        // RadioListTile(
+        //     title: const Text('Radio'),
+        //     value: true,
+        //     groupValue: true,
+        //     onChanged: (yes) => {}),
       ],
     );
   }
 }
 
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({
+    Key? key,
+    required this.init,
+  }) : super(key: key);
+  final ThemeMode init ;
 
+  @override
+  // State<ThemeModeSelectionPage> createState() => _ThemeModeSelectionPageState();
+  _ThemeModeSelectionPageState createState() => _ThemeModeSelectionPageState();
+}
 
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  late ThemeMode _current;
 
+  @override
+  void initState() {
+    super.initState();
+    _current = widget.init;//statefullwidgetで受け取った値を使う場合widget.xxxを使う
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          ListTile(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () =>
+                  Navigator.pop<ThemeMode>(context, _current),
+            ),
+          ),
+          RadioListTile<ThemeMode>(
+              title: const Text('System'),
+              value: ThemeMode.system,
+              groupValue: _current,
+              onChanged: (val) => {setState(() => _current = val!)}),
+          RadioListTile<ThemeMode>(
+              title: const Text('Dark'),
+              value: ThemeMode.dark,
+              groupValue: _current,
+              onChanged: (val) => {setState(() => _current = val!)}),
+          RadioListTile<ThemeMode>(
+              title: const Text('Light'),
+              value: ThemeMode.light,
+              groupValue: _current,
+              onChanged: (val) => {setState(() => _current = val!)}),
+        ],
+      )),
+    );
+  }
+}
+
+int modeToVal(ThemeMode mode) {
+  switch (mode) {
+    case ThemeMode.system:
+      return 1;
+    case ThemeMode.dark:
+      return 2;
+    case ThemeMode.light:
+      return 3;
+    default:
+      return 0;
+  }
+}
+
+ThemeMode valToMode(int val) {
+  switch (val) {
+    case 1:
+      return ThemeMode.system;
+    case 2:
+      return ThemeMode.dark;
+    case 1:
+      return ThemeMode.light;
+    default:
+      return ThemeMode.system;
+  }
+}
+
+Future<void> saveThemeMode(ThemeMode mode) async {
+  final pref = await SharedPreferences.getInstance();
+  pref.setInt('theme_mode', modeToVal(mode));
+}
+
+Future<ThemeMode> loadThemeMode() async {
+  final pref = await SharedPreferences.getInstance();
+  final ret = valToMode(pref.getInt('theme_mode') ??
+      0); // ??の左がnullの場合、右の変数を返す。左がnullでなければその時点でその変数を返す。
+  return ret;
+}
