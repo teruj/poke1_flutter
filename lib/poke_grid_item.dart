de5f903d30c01c64e28f314902ff952fa48f0a38
teruj <tjibu@msn.com>
2022-04-18 14:36:25 +0900

    step10_1_finish

---
+++ lib/poke_grid_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:poke/const/pokeapi.dart';
+import 'package:poke/models/pokemon.dart';
+import 'package:poke/poke_detail.dart';
+
+import 'const/pokeapi.dart';
+
+class PokeGridItem extends StatelessWidget {
+  const PokeGridItem({Key? key, required this.poke}) : super(key: key);
+  final Pokemon? poke;
+
+  @override
+  Widget build(BuildContext context) {
+    if (poke != null) {
+      return Column(
+        children: [
+          InkWell(
+            onTap: () => {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) {
+                return PokeDetail(poke: poke!);
+              }))
+            },
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: (pokeTypeColors[poke!.types.first] ?? Colors.grey[100])!
+                    .withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  fit: BoxFit.fitWidth,
+                  image: NetworkImage(poke!.imageUrl),
+                ),
+              ),
+            ),
+          ),
+          Text(
+            poke!.name,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
+      );
+    } else {
+      return const SizedBox(
+        width: 100,
+        height: 100,
+        child: Center(
+          child: Text('...'),
+        ),
+      );
+    }
+  }
+}
--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -5,6 +5,7 @@ import 'poke_list_item.dart';
 import './models/pokemon.dart';
 
 import './models/favorite.dart';
+import 'poke_grid_item.dart';
 
 class PokeList extends StatefulWidget {
   const PokeList({Key? key}) : super(key: key);
@@ -18,6 +19,8 @@ class _PokeListState extends State<PokeList> {
 
   bool isFavoriteMode = false;
   // bool isFavoriteMode = true;
+
+  bool isGridMode = true;
 
   int _currentPage = 1;
 
@@ -56,9 +59,15 @@ class _PokeListState extends State<PokeList> {
     }
   }
 
-  void changeMode(bool currentMode) {
+  void changeFavMode(bool currentMode) {
     setState(() {
       isFavoriteMode = !currentMode;
+    });
+  }
+
+  void changeGridMode(bool currentMode) {
+    setState(() {
+      isGridMode = !currentMode;
     });
   }
 
@@ -72,7 +81,8 @@ class _PokeListState extends State<PokeList> {
             alignment: Alignment.topRight,
             child: IconButton(
               onPressed: () async {
-                var ret = await showModalBottomSheet<bool>(
+                // var ret = await showModalBottomSheet<bool>(
+                await showModalBottomSheet(
                     context: context,
                     shape: const RoundedRectangleBorder(
                       borderRadius: BorderRadius.only(
@@ -83,11 +93,14 @@ class _PokeListState extends State<PokeList> {
                     builder: (BuildContext context) {
                       return ViewModeBottomSheet(
                         favMode: isFavoriteMode,
+                        gridMode: isGridMode,
+                        changeFavMode: changeFavMode,
+                        changeGridMode: changeGridMode,
                       );
                     });
-                if (ret != null && ret) {
-                  changeMode(isFavoriteMode);
-                }
+                // if (ret != null && ret) {
+                //   changeMode(isFavoriteMode);
+                // }
               },
               icon: const Icon(Icons.auto_awesome_outlined),
             ),
@@ -97,29 +110,65 @@ class _PokeListState extends State<PokeList> {
               if (itemCount(favs.favs.length, _currentPage) == 0) {
                 return const Text('no data');
               } else {
-                return ListView.builder(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                  // itemCount: 898,
-                  itemCount: itemCount(favs.favs.length, _currentPage) + 1,
-                  itemBuilder: (context, index) {
-                    if (index == itemCount(favs.favs.length, _currentPage)) {
-                      return OutlinedButton(
-                        onPressed: isLastPage(favs.favs.length, _currentPage)
-                            ? null
-                            : () => {
-                                  setState(() {
-                                    _currentPage++;
-                                  })
-                                },
-                        child: const Text('more'),
+                if (isGridMode) {
+                  return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemCount: itemCount(favs.favs.length, _currentPage) + 1,
+                    itemBuilder: (context, index) {
+                      if (index == itemCount(favs.favs.length, _currentPage)) {
+                        return Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: OutlinedButton(
+                            child: const Text('more'),
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            onPressed:
+                                isLastPage(favs.favs.length, _currentPage)
+                                    ? null
+                                    : () => {
+                                          setState(() => _currentPage++),
+                                        },
+                          ),
+                        );
+                      } else {
+                        return PokeGridItem(
+                          poke: pokes.byId(itemId(favs.favs, index)),
+                        );
+                      }
+                      ;
+                    },
+                  );
+                } else {
+                  return ListView.builder(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                    // itemCount: 898,
+                    itemCount: itemCount(favs.favs.length, _currentPage) + 1,
+                    itemBuilder: (context, index) {
+                      if (index == itemCount(favs.favs.length, _currentPage)) {
+                        return OutlinedButton(
+                          onPressed: isLastPage(favs.favs.length, _currentPage)
+                              ? null
+                              : () => {
+                                    setState(() {
+                                      _currentPage++;
+                                    })
+                                  },
+                          child: const Text('more'),
+                        );
+                      }
+                      return PokeListItem(
+                        poke: pokes.byId(itemId(favs.favs, index)),
                       );
-                    }
-                    return PokeListItem(
-                      poke: pokes.byId(itemId(favs.favs, index)),
-                    );
-                  },
-                );
+                    },
+                  );
+                }
               }
             }),
           )
@@ -129,24 +178,24 @@ class _PokeListState extends State<PokeList> {
   }
 }
 
-
-
 class ViewModeBottomSheet extends StatelessWidget {
   const ViewModeBottomSheet({
     Key? key,
     required this.favMode,
+    required this.changeFavMode,
+    required this.gridMode,
+    required this.changeGridMode,
   }) : super(key: key);
   final bool favMode;
+  final Function(bool) changeFavMode;
+  final bool gridMode;
+  final Function(bool) changeGridMode;
 
   String mainText(bool fav) {
-    if (fav) {
-      return 'お気に入りのポケモンが表示されています';
-    } else {
-      return 'すべてのポケモンが表示されています';
-    }
+    return '表示設定';
   }
 
-  String menuTitle(bool fav) {
+  String menuFavTitle(bool fav) {
     if (fav) {
       return '「すべて」表示に切り替え';
     } else {
@@ -154,11 +203,27 @@ class ViewModeBottomSheet extends StatelessWidget {
     }
   }
 
-  String menuSubtitle(bool fav) {
+  String menuFavSubtitle(bool fav) {
     if (fav) {
-      return 'すべてのポケモンが表示されています';
+      return 'すべてのポケモンが表示されます';
     } else {
-      return 'お気に入りに登録したポケモンのみが表示されています';
+      return 'お気に入りに登録したポケモンのみが表示されます';
+    }
+  }
+
+  String menuGridTitle(bool grid) {
+    if (grid) {
+      return 'リスト表示に切り替え';
+    } else {
+      return 'グリッド表示に切り替え';
+    }
+  }
+
+  String menuGridSubtitle(bool grid) {
+    if (grid) {
+      return 'ポケモンをグリッド表示します';
+    } else {
+      return 'ポケモンをリスト表示します';
     }
   }
 
@@ -166,7 +231,7 @@ class ViewModeBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
         child: Center(
           child: Column(
             children: [
@@ -192,13 +257,27 @@ class ViewModeBottomSheet extends StatelessWidget {
               ListTile(
                 leading: const Icon(Icons.swap_horiz),
                 title: Text(
-                  menuTitle(favMode),
+                  menuFavTitle(favMode),
                 ),
                 subtitle: Text(
-                  menuSubtitle(favMode),
+                  menuFavSubtitle(favMode),
                 ),
                 onTap: () {
-                  Navigator.pop(context, true);
+                  changeFavMode(favMode);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.grid_3x3),
+                title: Text(
+                  menuGridTitle(gridMode),
+                ),
+                subtitle: Text(
+                  menuGridSubtitle(gridMode),
+                ),
+                onTap: () {
+                  changeGridMode(gridMode);
+                  Navigator.pop(context);
                 },
               ),
               OutlinedButton(
@@ -215,5 +294,3 @@ class ViewModeBottomSheet extends StatelessWidget {
         ));
   }
 }
-
-
